5d6e390a8e4b24778ecc3aac6c182104ab4f3a39
Narasimha Patro <suraj.cse.28@gmail.com>
2026-02-18 14:35:08 +0530

    data_ingestion.py structure coding & exception.py & logger.py logic update and updated gitignore fil

--- src/logger.py
+++ src/logger.py
@@ -2,23 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
-## Testing purpose
-
-## if __name__ == "__main__":
-##    logging.info("Logging has Started")
-
-# Create logger Object to use in another python file
-
-##logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
