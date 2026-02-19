394f2a74adc0a60f43c65aaa76f2ede6144e862a
Narasimha Patro <suraj.cse.28@gmail.com>
2026-02-19 12:00:22 +0530

    Added Hyperparameter in model_trainer.py file and updated utils.py file and updated the approach in the logger.py (To catch file location of Info details)

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,33 @@ import logging
 import os
 from datetime import datetime
 
-LOG_DIR = os.path.join(os.getcwd(), "logs")
-os.makedirs(LOG_DIR, exist_ok=True)
+class RelativePathFormatter(logging.Formatter):
+    def format(self, record):
+        # Normalize path for Windows/Linux
+        path = record.pathname.replace("\\", "/")
+
+        # Trim path to start from src/
+        if "/src/" in path:
+            record.pathname = path.split("/src/", 1)[1]
+            record.pathname = f"src/{record.pathname}"
+
+        return super().format(record)
+
 
 LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+handler = logging.FileHandler(LOG_FILE_PATH)
+formatter = RelativePathFormatter(
+    '[%(asctime)s] [%(pathname)s:%(lineno)d] %(levelname)s - %(message)s'
 )
 
-logger = logging.getLogger(__name__)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logger
@@ -21,28 +22,37 @@ def save_object(file_path, obj):
     except Exception as ex:
         raise CustomException(ex, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
+        trained_models = {}
 
-        for i in range(len(list(models))):
-            # Individual Model
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            param = params.get(model_name, {})
 
-            # Train Model
-            model.fit(X_train, y_train)
+            # CatBoost WITHOUT GridSearchCV
+            if model_name == "CatBoosting Regressor":
+                logger.info("Training CatBoost without GridSearchCV")
+                model.fit(X_train, y_train)
+            else:
+                gs = GridSearchCV(model, param, cv=3)
+                gs.fit(X_train, y_train)
 
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+
+            # Predictions
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            # R2_Scores
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
 
-            test_model_score = r2_score(y_test, y_test_pred)
+            report[model_name] = test_score
+            trained_models[model_name] = model
 
-            report [list(models.keys())[i]] = test_model_score
-        
-        return report
-    
+        return report, trained_models
+
     except Exception as ex:
         raise CustomException(ex, sys)
